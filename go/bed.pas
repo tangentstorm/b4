8d0d850a6e2923ba34c9739f68f55ba027f9a7bf
Michal Wallace <michal@sabren.com>
2012-08-23 13:27:57 -0500

    cleaned up the coordinates and keyboard codes, added backspace

--- go/bed.pas
+++ go/bed.pas
@@ -7,7 +7,7 @@
 | copyright (c) 2012 michal j. wallace
 | see LICENSE.org for usage information
 }
-{$gnu-pascal}
+
 program bed;
 uses crt, gpcutil;
 
@@ -33,25 +33,30 @@ begin
     crt.kbRIGHT: inc( gx );
     crt.kbESC  : gdone := true;
 
-    13         : begin
+    crt.kbCR   : begin
                    gx := 1;
                    inc( gy );                   
                  end;
     
-    { the 16x make option-x act like fx on OSX panther }
-    crt.kbf1, 161   : crt.textcolor( crt.blue );
-    crt.kbf2, 162   : crt.textcolor( crt.green );
-    crt.kbf3, 163   : crt.textcolor( crt.cyan );
-    crt.kbf4, 164   : crt.textcolor( crt.red );
-    crt.kbf5, 165   : crt.textcolor( crt.magenta );
-    crt.kbf6, 166   : crt.textcolor( crt.brown );
-    crt.kbf7, 167   : crt.textcolor( crt.lightgray );
+    crt.kbBkSp : begin
+                   dec( gx );
+                   crt.gotoxy( gx, gy );
+                   crt.writechar( ' ' );
+                 end;
+    
+    crt.kbf1   : crt.textcolor( crt.blue );
+    crt.kbf2   : crt.textcolor( crt.green );
+    crt.kbf3   : crt.textcolor( crt.cyan );
+    crt.kbf4   : crt.textcolor( crt.red );
+    crt.kbf5   : crt.textcolor( crt.magenta );
+    crt.kbf6   : crt.textcolor( crt.brown );
+    crt.kbf7   : crt.textcolor( crt.lightgray );
   else
     begin
       crt.writechar( crt.key2char( key ));
       inc( gx );
-      crt.writestring( '     ', 1 , 1 );
-      crt.writestring( int2str( key ), 1 , 1 );
+      crt.writestring( '     ', 1 , 65 );
+      crt.writestring( int2str( key ), 1, 65 );
     end;
   end;
   
@@ -70,32 +75,19 @@ begin
   crt.textcolor( crt.lightgray );
   for y := 0 to kh do
   begin
-    crt.gotoxy( 4, y + 2 );
-    for x := 0 to kw do
-    begin
-      write( ' ' );
-    end;
+    crt.gotoxy( 1, y);
+    crt.clreol;
   end;
-end;
-
-procedure banner;
-begin
-  crt.gotoxy( 1, 1 );
-  crt.textbackground( crt.blue );
-  crt.clrscr;
-  crt.textbackground( crt.lightgray );
-  crt.clreol;
 end;
 
 
 begin
   crt.SetCRTUpdate( crt.UpdateAlways );
-  banner();
   
   load( gbuf );
   gx := 1; gy := 1;
   repeat
-     crt.gotoxy( gx +3, gy+1 );
+     crt.gotoxy( gx, gy );
      if crt.keypressed then handle_key;
    until gdone;
 end.
