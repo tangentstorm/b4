4f40cd71d2bf390643af96c224fea8fa5f4d954e
Michal Wallace <michal@sabren.com>
2012-08-23 15:59:29 -0500

    much keyboard cleanup, much time wasted trying to get non-bright colors in panther's terminal :/

--- go/bed.pas
+++ go/bed.pas
@@ -7,7 +7,6 @@
 | copyright (c) 2012 michal j. wallace
 | see LICENSE.org for usage information
 }
-
 program bed;
 uses crt, gpcutil;
 
@@ -22,68 +21,89 @@ var
   gdone : boolean;
 
 procedure handle_key;
-  var key : crt.tkey;
+var
+  key : char;
 begin
-  key := crt.readkeyword;
+  key := crt.readkey;
+  
   case key of
     
-    crt.kbLEFT : dec( gx );
-    crt.kbUP   : dec( gy );
-    crt.kbDOWN : inc( gy );
-    crt.kbRIGHT: inc( gx );
-    crt.kbESC  : gdone := true;
-
-    crt.kbCR   : begin
+    #0 : begin
+           { capture key again so we can debug the codes }
+           key := crt.readkey;
+           case key of
+             { option-f1 through option-f5 is all
+               i could get to work on panther... }
+             #64 : crt.textattr := $07;
+             #65 : crt.textcolor( crt.red );
+             #66 : crt.textcolor( crt.green );
+             #67 : crt.textcolor( crt.yellow );
+             #68 : crt.textcolor( crt.cyan );
+             crt.ksLEFT : dec( gx );
+             crt.ksUP   : dec( gy );
+             crt.ksDOWN : inc( gy );
+             crt.ksRIGHT: inc( gx );
+           else
+           end
+         end;
+    
+    crt.chESC  : gdone := true;
+    crt.chCR   : begin
                    gx := 1;
                    inc( gy );                   
                  end;
-    
-    crt.kbBkSp : begin
+    crt.chBkSp : begin
                    dec( gx );
                    crt.gotoxy( gx, gy );
                    crt.writechar( ' ' );
                  end;
-    
-    crt.kbf1   : crt.textcolor( crt.blue );
-    crt.kbf2   : crt.textcolor( crt.green );
-    crt.kbf3   : crt.textcolor( crt.cyan );
-    crt.kbf4   : crt.textcolor( crt.red );
-    crt.kbf5   : crt.textcolor( crt.magenta );
-    crt.kbf6   : crt.textcolor( crt.brown );
-    crt.kbf7   : crt.textcolor( crt.lightgray );
   else
     begin
-      crt.writechar( crt.key2char( key ));
+      crt.writechar( key );
       inc( gx );
-      crt.writestring( '     ', 1 , 65 );
-      crt.writestring( int2str( key ), 1, 65 );
     end;
   end;
   
   if crt.gettextcolor = crt.black
   then crt.textbackground( crt.lightgray )
   else crt.textbackground( crt.black );
+
+  { debug display of keypress / color: }
+  crt.gotoxy( 67, 1 );
+  write( '[    ]');
+  crt.gotoxy( 68, 1 );
+  write( ord( key ));
   
   gx := min( kw, max( gx, 1 ));
   gy := min( kh, max( gy, 1 ));
 end;
 
 procedure load( buf : array of char );
-  var x, y : byte;
+var x, y : byte;
 begin
-  crt.textbackground( crt.black );
-  crt.textcolor( crt.lightgray );
+end;
+
+procedure init;
+var y : byte;
+begin
+  crt.textattr := $17;
+  crt.clrscr;
   for y := 0 to kh do
-  begin
-    crt.gotoxy( 1, y);
-    crt.clreol;
-  end;
+    begin
+      crt.textattr := $07;
+      crt.gotoxy( 1, y);
+      crt.clreol;
+      crt.gotoxy( 65, y);
+      crt.textattr := $13;
+      write( '|' );
+      crt.clreol;
+    end;
+  crt.textattr := $07;
 end;
 
 
 begin
-  crt.SetCRTUpdate( crt.UpdateAlways );
-  
+  init;
   load( gbuf );
   gx := 1; gy := 1;
   repeat
