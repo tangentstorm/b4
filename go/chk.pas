12144e7a4c4153248a5d084e64741359f489e273
Michal Wallace <michal@sabren.com>
2012-08-29 02:35:14 -0500

    added a little report to the test harness

--- go/chk.pas
+++ go/chk.pas
@@ -6,8 +6,11 @@ type str = string[ 255 ];
 procedure fail( const msg : str );
 procedure test( pred : boolean; const msg : str );
 procedure equal( a, b : int; const msg : str );
-
+procedure report;
+  
 implementation
+
+var count : int = 0;  
 
 procedure pass;
 begin
@@ -24,11 +27,20 @@ end;
 procedure test( pred : boolean; const msg : str );
 begin
   if pred then pass else fail( msg );
+  inc( count );
 end;
 
 procedure equal(a, b : int; const msg : str );
 begin
-  if a <> b then fail( msg );
+  test( a = b, msg );
+end;
+
+procedure report;
+begin
+  writeln;
+  writeln;
+  write( count );
+  writeln( ' tests passed.' );
 end;
 
 end.
--- go/test_sd.pas
+++ go/test_sd.pas
@@ -5,4 +5,5 @@ var drive : sd.tDrive;
 begin
   drive.init( 'test_sd.b4sd' );
   chk.equal( drive.size, 0, 'drive should be empty' );
+  report;
 end.
