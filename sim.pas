85d8ae58a228ba11dd62de8ee6a115693e79d2e7
tangentstorm <michal.wallace@gmail.com>
2012-09-28 14:00:35 -0500

    ditched the hardware type. sim does nothing atm, but i've still got plans for the other interfaces here

--- sim.pas
+++ sim.pas
@@ -16,22 +16,8 @@ interface uses xpc;
       property visible : boolean;
       procedure show;
     end;     
-    hardware = object
-      constructor init;
-      function send( msg : int32 ) : int32; virtual;
-    end;		
 
 
 implementation
-
-  constructor hardware.init;
-  begin
-  end; { hardware.init }
-  
-  function hardware.send( msg: int32 ): int32;
-  begin
-    result := 0;
-  end; { hardware.send }
-
 
 end.
