47916afa9ab63536529fbf7c60e6b38a99d6aecb
tangentstorm <michal.wallace@gmail.com>
2012-09-29 19:20:21 -0500

    brought test for hex() in line with the code

--- test_xpc.pas
+++ test_xpc.pas
@@ -1,18 +1,8 @@
 {$i xpc.inc }
 program test_xpc;
   uses xpc, math;
-
-
   var x : integer;
 begin
-
-{  for x := 0 to 8 do
-  begin
-    writeln( '2^', x, '=', 2**x, ' = ', 1 shl x, ' = $', hex( 1 shl x )); 
-  end;
- } 
-  
-  if hex(1234) <> ansistring('00 00 04 D2') then
-    writeln( 'hex broke. says hex(1234) = ', hex(1234), '.' );
-  
+  if hex(1234) = ansistring('4D2') then writeln('ok.')
+  else writeln( 'no: hex says hex(1234) = "', hex(1234), '". Should be "4D2"' );
 end.
