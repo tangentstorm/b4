e1bd2b56cadcefd36ad93c374d8f633f753b812a
tangentstorm <michal.wallace@gmail.com>
2012-11-07 06:08:16 -0600

    big ugly parser for function defs

--- pre/pre_gen.py
+++ pre/pre_gen.py
@@ -5,18 +5,71 @@ import sys, doctest
 def extract_sig( line ):
     """
     :: pascal function declaration
-    -> ( name:str, type:str, args:[( name:str, type:str )])
+    -> ( name:str, type:str, args:[( kwd:str, [ name:str ], type:str )])
 
-    Limited to simple types and doesn't support arglists with
-    the same type ( ie, this fails on "a, b: integer" )
+    Limited to simple types and doesn't support default values yet.
 
-    >>> extract_sig( "function err( msg : string ) : pattern;" )
-    ('err', 'pattern', [('msg','string')])
-
+    >>> extract_sig( "function abc( a, b : int; var c : str ) : obj;" )
+    ('abc', 'obj', [('', ['a', 'b'], 'int'), ('var', ['c'], 'str')])
     """
+    res = [ ]
+    tokens = [ tok.strip( ) for tok in line.lower( )
+              .replace( ';', ' ' )
+              .replace( '(', ' ' )
+              .replace( ',', ' ' )
+              .replace( ')', ' ' )
+              .strip( )
+              .split( )]
+
+    # little stack machine to help with parsing:
+    class the : list = res # just an arbitrary namespace
+    def keep( v ): the.list.append( v )
+    def next( index = 0 ):
+        the.token = tokens.pop( index )
+        return the.token
+    def last( ): return next( -1 )
+    def drop( v ): pass
+
+    # extract the name and return type, leaving just the args:
+    if next( ) == 'function':
+        keep( next( ))  # function name
+        keep( last( ))  # return type
+        drop( last( ))  # the ":" before the return type
+    else:
+        keep( next( ))  # procedure name
+        keep( None )    # no return type for procedures
+
+    # at this point, tokens is either empty,
+    # or it contains the argument list
+    args = [ ]
+    keep( args )
+    while tokens:
+
+        the.list = arg_group = [ ] # start a new list for keep( )
+        if next( ) in ( 'var', 'const' ):
+            keep( the.token )
+            next( )
+        else: keep( '' )  # no var/const keyword
+
+        the.list = names = [ ]
+        keep( the.token ) # just keep the name
+
+        while next( ) != ':' :
+            keep( the.token ) # names in the list
+
+        # now the.token == : , next up is the type for the group
+        drop( the.token )
+        the.list = arg_group
+        keep( names ) # leave it as a list just to alternate the brackets
+        keep( next( ))
+
+        the.list = args
+        keep( tuple( arg_group ))
+
+    return tuple( res )
 
 def gen_code_for( line ):
-    name, args = extract_sig( line )
+    name, type, args = extract_sig( line )
     #  TODO : actually generate the code ;)
 
 if __name__=="__main__":
