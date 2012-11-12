a0a96913b601f9c46155a4adf9c7950e5f17d02a
tangentstorm <michal.wallace@gmail.com>
2012-11-12 16:15:28 -0600

    moved ( xpc, xpc_str, chk, log, utf8, posix, stacks, xterm256color, test_xpc, xpc.inc ) to new xpl repository

--- test_xpc.pas
+++
@@ -1,8 +0,0 @@
-{$i xpc.inc }
-program test_xpc;
-  uses xpc, math;
-  var x : integer;
-begin
-  if hex(1234) = ansistring('4D2') then writeln('ok.')
-  else writeln( 'no: hex says hex(1234) = "', hex(1234), '". Should be "4D2"' );
-end.
