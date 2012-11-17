4671df7ec51d9e21404e879f722959f9c8599e36
tangentstorm <michal.wallace@gmail.com>
2012-11-17 15:28:29 -0600

    added grin ( grammar interpreter ) from two nights ago

---
+++ pre/grin.pas
@@ -0,0 +1,51 @@
+{$i xpc.inc }
+program grin;
+uses xpc, pre, stacks;
+
+  type
+    FileSource = class ( TInterfacedObject, ISource )
+      constructor create( path : string );
+      procedure next( ch : char );
+      procedure mark(var mk : Marker );
+      procedure back(var mk : Marker );
+      procedure keep;
+    private
+      hnd : file of char;
+    end;
+
+  constructor filesource.create( path : string );
+  begin
+  end;
+
+  procedure filesource.next( ch : char );
+  begin
+    read( self.hnd, ch );
+  end;
+
+  procedure filesource.mark( var mk : Marker );
+  begin
+
+  end;
+
+  procedure filesource.back( var mk : Marker );
+  begin
+  end;
+
+  procedure filesource.keep;
+  begin
+  end;
+
+
+  procedure dumpAST( m :  matcher );
+  begin
+  end;
+
+
+var
+  m : matcher;
+  f : filesource;
+begin
+  m := matcher.create;
+  f := filesource.create( 'oberon0.ebnf' );
+  dumpAST( m );
+end.
