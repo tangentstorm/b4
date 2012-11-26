82d52fed7459228c03f6b259233c0860e6a4e0ba
tangentstorm <michal.wallace@gmail.com>
2012-11-26 16:27:30 -0600

    test_pre: fixed names and arguments

--- pre/test_pre.pas
+++ pre/test_pre.pas
@@ -3,13 +3,16 @@ implementation uses pre;
 
   procedure test_nul;
   begin
-    chk.that( nul.matches( '' ));
+    chk.that( nul.matches( '<anything>' ),
+	     'nul should always match.' );
   end;
 
   procedure test_lit;
   begin
-    chk.that( rx( 'a' ).matches( 'apple' ));
-    chk.that( not rx( 'a' ).matches( 'banana' ));
+    chk.that( lit( 'a' ).matches( 'apple' ),
+	     'lit.match false negative' );
+    chk.that( not lit( 'a' ).matches( 'banana' ),
+	     'lit.match false positive' );
   end;
 
 
