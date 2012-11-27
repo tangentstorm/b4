d3bdd83119435e22b59bdc8d98c7a9eb5d1e93c8
tangentstorm <michal.wallace@gmail.com>
2012-11-26 22:33:25 -0600

    pre: removed duplication from tests

--- pre/test_pre.pas
+++ pre/test_pre.pas
@@ -3,36 +3,48 @@ implementation uses pre;
 
   var pat : pattern;
 
+
+  procedure should_match( s : string );
+  begin
+    chk.that( pat.matches( s ), 'false negative:' + s );
+  end;
+
+  procedure should_not_match( s : string );
+  begin
+    chk.that( not pat.matches( s ), 'false positive:' + s );
+  end;
+
+
+
   procedure test_nul;
   begin
-    chk.that( nul.matches( '<anything>' ),
-	     'nul should always match.' );
+    chk.that( nul.matches( '<anything>' ), 'nul should always match.' );
   end;
 
   procedure test_sym;
   begin
     pat := sym( 'a' );
-    chk.that( pat.matches( 'apple' ), 'lit.match false negative' );
-    chk.that( not pat.matches( 'banana' ), 'lit.match false positive' );
+    should_match( 'apple' );
+    should_not_match( 'banana' );
   end;
 
   procedure test_any;
   begin
-    pat := any([ 'a', 'b', 'c']);
-    chk.that( pat.matches( 'apple' ), 'any.match false negative: apple' );
-    chk.that( pat.matches( 'banana' ), 'any.match false negative: apple' );
-    chk.that( pat.matches( 'cherry' ), 'any.match false negative: apple' );
-    chk.that( not pat.matches( 'durian' ), 'lit.match false positive' );
+    pat := any([ 'a', 'b', 'c' ]);
+    should_match( 'apple' );
+    should_match( 'banana' );
+    should_match( 'cherry' );
+    should_not_match( 'durian' );
   end;
 
   procedure test_lit;
   begin
     pat := lit( 'app' );
-    chk.that( pat.matches( 'apple' ), 'any.match false negative: apple' );
-    chk.that( pat.matches( 'application' ), 'any.match false negative: apple' );
-    chk.that( not pat.matches( 'apropos' ), 'any.match false positive: apropos' );
-    chk.that( not pat.matches( 'bumblebee' ), 'lit.match false positive: bumblebee' );
-    chk.that( not pat.matches( ' apple' ), 'any.match false positive: [space]apple' );
+    should_match( 'apple' );
+    should_match( 'application' );
+    should_not_match( 'apropos' );
+    should_not_match( 'bumblebee' );
+    should_not_match( ' apple' );
   end;
 
 
