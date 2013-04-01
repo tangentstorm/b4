a3686294f6f4702707b13c304cee0fd7706da443
tangentstorm <michal.wallace@gmail.com>
2013-04-01 03:26:33 -0500

    rxgl_zen: run the SDL terminal inside the ZenGL mainloop.

--- ng/rxgl_zen.pas
+++ ng/rxgl_zen.pas
@@ -1,6 +1,15 @@
 {$mode objfpc}
 unit rxgl_zen;
-interface uses ng, SDL, sysutils, rt_term;
+interface uses ng, SDL, sysutils, rt_term,
+  zgl_main,
+  zgl_types,
+  zgl_window,
+  zgl_screen,
+  zgl_textures,
+  zgl_keyboard,
+  zgl_sprite_2d,
+  zgl_timers,
+  zgl_utils;
 
   procedure Main( rxvm : ng.TRetroVM );
 
@@ -119,14 +128,44 @@ begin
 end;
 }
 
-procedure Main( rxvm : ng.TRetroVM );
-  var vdp : TSDLVDP;
+var
+  vm : ng.TRetroVM;
+  vt : TSDLVDP;
+
+procedure OnLoad;
 begin
-  vdp := TSDLVDP.Create;
-  vdp.Attach( rxvm );
-  while not rxvm.done do rxvm.tick;
-  vdp.Destroy;
+  vt.Attach( vm );
+end;
+
+procedure OnStep;
+  var i : integer = 0;
+  const opsPerStep = 65535; { arbitrary number }
+begin
+  repeat
+    inc(i); vm.tick {todo: raname to step }
+  until (i = opsPerStep) or vm.done;
+  if vm.done then zgl_exit
+end;
+
+procedure OnExit;
+begin
+  vt.Destroy;
+end;
+
+procedure Main( rxvm : ng.TRetroVM );
+begin
+  vt := TSDLVDP.Create;
+  vm := rxvm;
+  zgl_init;
 end;
 
 initialization
+  zgl_Disable( APP_USE_LOG );
+  zgl_Disable( APP_USE_AUTOPAUSE );
+  zgl_reg( SYS_LOAD,   @OnLoad);
+  zgl_reg( SYS_UPDATE, @OnStep);
+//  zgl_reg( SYS_DRAW,   @OnDraw);
+//  zgl_reg( INPUT_KEY_CHAR, @OnChar );
+  zgl_reg( SYS_EXIT,   @OnExit);
+  scr_SetOptions( 800, 600, REFRESH_MAXIMUM, {fullscreen=}false, {vsync=}true );
 end.
