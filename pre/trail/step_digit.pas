3e15e7ac6c7991b9b0f36f533d81844c5ff00f12
tangentstorm <michal.wallace@gmail.com>
2013-04-19 01:03:59 -0500

    fixed digit match

--- pre/trail/step_digit.pas
+++ pre/trail/step_digit.pas
@@ -2,13 +2,13 @@
 program step_digit;
 uses parsetrail;
 
-  const
-    digits : set of char = [ '0'..'9' ];
+const
+  digits : set of char = [ '0'..'9' ];
 
-  function match_digit( s : string ) : boolean;
-  begin
-    result := s[ 0 ] in digits;
-  end;
+function match_digit( s : string ) : boolean;
+begin
+  result := (s[ 1 ] in digits) and (length( s ) = 1);
+end;
   
 begin
   parsetrail.match_loop( @match_digit );
