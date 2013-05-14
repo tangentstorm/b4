c7da40638cbf39dd74c818d5c41d9260f84b7161
tangentstorm <michal.wallace@gmail.com>
2013-05-14 04:43:03 -0500

    bed: cleaned up the coordinates a while back

--- go/bed.pas
+++ go/bed.pas
@@ -28,9 +28,9 @@ var
   key : char;
 begin
   key := kbd.readkey;
-  
+
   case key of
-    
+
     #0 :
       begin
         key := kbd.readkey;
@@ -39,7 +39,7 @@ begin
 	  kbd.UP    : dec( gy );
 	  kbd.DOWN  : inc( gy );
 	  kbd.RIGHT : inc( gx );
-          
+
           { colors : $08 indicates bright }
 	  kbd.F1 : vt.fg( ord(vt.red) + brightflag );
 	  kbd.F2 : vt.fg( ord(vt.grn) + brightflag );
@@ -55,37 +55,38 @@ begin
 		     vt.fg( vt.textattr xor 8 );
 		   end
         end;
-        { black text is dark gray, OR black on light gray, depending on bit 8 }
+        { black text is dark gray, OR black on gray, depending on bit 8 }
 	if lo(vt.textattr) = ord( vt.blk )
 	  then vt.bg( ord( vt.blk ) + 8 )
           else vt.bg( ord( vt.blk ))
       end;
-    
+
     ascii.ESC  : gdone := true;
     ascii.CR   : begin
-                   gx := 1;
+                   gx := 0;
                    inc( gy );
                  end;
-    ascii.BS   : begin
-                   dec( gx );
-                   vt.gotoxy( gx, gy );
-		   write(' ');
-                   gBuf[ (( gy - 1 ) * 64 ) + ( gx - 1 ) ] := 32;
-                 end;
+    ascii.BS   : if gx > 0 then
+		   begin
+		     dec( gx );
+		     vt.gotoxy( gx, gy );
+		     write(' ');
+		     gBuf[ gy * 64 + gx] := 32;
+		   end;
   else
     begin
-      gBuf[ (( gy - 1 ) * 64 ) + ( gx - 1 ) ] := ord( key );
+      gBuf[ gy * 64 + gx ] := ord( key );
       write( key );
       inc( gx );
     end;
   end;
-  
+
   { debug display of keypress / color: }
-  vt.gotoxy( 67, 1 );
+  vt.gotoxy( 67, 0 );
   write( '[    ]');
-  vt.gotoxy( 68, 1 );
+  vt.gotoxy( 68, 0 );
   write( ord( key ));
-  
+
   gx := min( kw, max( gx, 0 ));
   gy := min( kh, max( gy, 1 )); { 2 because of title bar }
 end;
@@ -127,10 +128,10 @@ begin
 
   vt.fg( $17 );
   vt.clrscr;
-  
+
   for y := 0 to 15 do
     begin
-      vt.gotoxy( 1, y+1 );
+      vt.gotoxy( 0, y );
       if y = 0 then write_line( y, $70, $60 )
       else write_line( y, $07, $40 );
       vt.fg( $13 );
@@ -138,7 +139,7 @@ begin
       vt.clreol;
     end;
   vt.fg( $07 );
-  gx := 1; gy := 2;
+  gx := 0; gy := 1;
 end;
 
 
