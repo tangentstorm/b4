5b36ca6f9a79e4ed132e28b7910ac29c1e141a67
tangentstorm <michal.wallace@gmail.com>
2013-07-05 16:33:00 -0500

    bed: actually load and save blocks {jul 5}

--- go/bed.pas
+++ go/bed.pas
@@ -19,12 +19,36 @@ const
 var
   gDrive     : sd.tDrive;
   gBuf	     : sd.tBlock;
+  gBlockNum  : word = 0;
   gx, gy     : byte;
   gDone	     : boolean;
   brightFlag : boolean = false;
   color	     : char;
 
   const infoColumn = 67;
+
+
+procedure make_block;
+  begin
+    gDrive.grow( 1 );
+    fillchar(gBuf, sizeOf(gBuf), $87);
+    // go back and make first line reversed:
+    fillchar(gBuf, 64, $88);
+  end;
+
+procedure load_data;
+  begin
+    gDrive.init( 'blocks.sd' );
+    if gDrive.block_count = 0 then
+      make_block
+    else gDrive.load( 0, gBuf );
+  end;
+
+procedure save_data;
+  begin
+    gDrive.save( 0, gBuf );
+    write('SAVING');
+  end;
 
 procedure fg( ch : char );
   begin
@@ -36,6 +60,29 @@ procedure ToggleBright;
     if brightFlag then Fg( LowerCase( color ))
     else Fg( UpCase( color ));
     brightFlag := not brightFlag;
+  end;
+
+procedure emit( ch : char );
+  begin
+    if ch = #$88 then
+      begin
+	kvm.fg( 'k' ); kvm.bg( 'w' );
+	write( '.' )
+      end
+    else if ch > #$80 then
+      begin
+	kvm.bg( 'k' );
+	kvm.fg( ord(ch) - $80 );
+	write( '.' );
+      end
+    else write( ch )
+  end;
+
+procedure addChar( ch : char );
+  begin
+    gBuf[ gy * 64 + gx ] := ord( ch );
+    emit( ch );
+    inc( gx );
   end;
 
 procedure handle_key;
@@ -50,27 +97,26 @@ begin
       begin
         key := kbd.readkey;
         case key of
-	  ^C : gDone := true;
-	  ^B, kbd.LEFT  : if gx > 0 then dec( gx );
-	  ^P, kbd.UP    : if gy > 0 then dec( gy );
-	  ^N, kbd.DOWN  : inc( gy );
-	  ^F, kbd.RIGHT : inc( gx );
+	  kbd.LEFT  : if gx > 0 then dec( gx );
+	  kbd.UP    : if gy > 0 then dec( gy );
+	  kbd.DOWN  : inc( gy );
+	  kbd.RIGHT : inc( gx );
 
           { colors }
-	  kbd.F1 : Fg( 'r' );
-	  kbd.F2 : Fg( 'g' );
-	  kbd.F3 : Fg( 'y' );
-	  kbd.F4 : Fg( 'b' );
-	  kbd.F5 : Fg( 'm' );
-	  kbd.F6 : Fg( 'c' );
-	  kbd.F7 : Fg( 'w' );
-	  kbd.F8 : Fg( 'k' );
+	  kbd.F1 : fg('r');
+	  kbd.F2 : fg('g');
+	  kbd.F3 : fg('y');
+	  kbd.F4 : fg('b');
+	  kbd.F5 : fg('m');
+	  kbd.F6 : fg('c');
+	  kbd.F7 : fg('w');
+	  kbd.F8 : fg('k');
 
           { f9 toggles the brightness bit. }
 	  kbd.F9 : ToggleBright;
 	end;
         { black text is dark gray, OR black on gray, depending on bit 8 }
-	if color = 'k' then kvm.bg( 'K' ) else kvm.bg( 'k' );
+	if color = 'k' then kvm.bg( 'w' ) else kvm.bg( 'k' );
       end;
 
     ^A : gx := 0;
@@ -81,6 +127,7 @@ begin
     ^P : if gy > 0 then dec( gy );
     ^N : inc( gy );
     ^F : inc( gx );
+    ^S : save_data;
     ascii.CR   : begin
                    gx := 0; inc( gy );
                  end;
@@ -90,13 +137,9 @@ begin
 		     kvm.gotoxy( gx, gy );
 		     write(' ');
 		     gBuf[ gy * 64 + gx ] := 32;
-	           end;
-  else
-    begin
-      gBuf[ gy * 64 + gx ] := ord( key );
-      write( key );
-      inc( gx );
-    end;
+		   end;
+  else if key in [ #32 .. #127 ] then
+    addchar( key )
   end;
 
   { debug display of keypress / color: }
@@ -111,35 +154,16 @@ begin
 end;
 
 
-procedure write_line( lineno : integer; txtcolor, symcolor : byte );
-var i, b : byte;
+procedure write_line( lineno : integer );
+var x, b : byte;
 begin
-  for i := 0 to kw-1 do
+  for x := 0 to kw-1 do
   begin
-    b := gBuf[ ( lineno * kw ) + i ];
-    if b < 32 then begin
-      kvm.fg( symcolor );
-      write( ' ' );
-    end
-    else begin
-      kvm.fg( txtcolor );
-      write( chr( b ) );
-    end;
+    b := gBuf[ ( lineno * kw ) + x ];
+    emit( chr( b ));
   end;
 end;
 
-
-procedure load_data;
-begin
-  gDrive.init( 'blocks.sd' );
-  if gDrive.block_count = 0 then gDrive.grow( 1 );
-  gDrive.load( 0, gBuf );
-end;
-
-procedure save_data;
-begin
-  gDrive.save( 0, gBuf );
-end;
 
 procedure init_view;
 var y : byte;
@@ -151,11 +175,11 @@ begin
 
   for y := 0 to 15 do
     begin
+      if y = 0 then begin fg('k'); bg('w') end
+      else begin bg('k'); fg('w') end;
       kvm.gotoxy( 0, y );
-      if y = 0 then write_line( y, $70, $60 )
-      else write_line( y, $07, $40 );
-      kvm.fg( $13 );
-      write( '|' );
+      write_line( y );
+      kvm.fg( 'b' ); bg('k'); write( '|' );
       kvm.clreol;
     end;
   kvm.fg( $07 );
