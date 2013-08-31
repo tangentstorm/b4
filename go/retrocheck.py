7d02105fffe12caecbab1d95c83aa6709e147190
tangentstorm <michal.wallace@gmail.com>
2013-08-31 13:48:51 -0500

    got retrocheck to run cleanly and added some comments

--- go/retrocheck.py
+++ go/retrocheck.py
@@ -1,8 +1,15 @@
+"""
+This is a start on a type checker for retro and other forth dialects.
+
+The idea was to start with a small database of forth primitives
+and their stack effects, and then use that database to derive the
+stack effect for defined words.
+"""
 import unittest
 from narrative import testcase
 from collections import namedtuple
 
-Def = namdetuple('Def', [name, takes, gives, effects, doc])
+Def = namedtuple('Def', ['name', 'takes', 'gives', 'effects', 'doc'])
 
 types = {
     "A" : "address",
@@ -16,20 +23,20 @@ types = {
 
 words = [
 
-    Def('+', ['II'], ['I'],  [], 'z+y'),
-    Def('-', ['II'], ['I'],  [], 'z-y'),
-    Def('*', ['II'], ['I'],  [], 'z*y'),
-    Def('%', ['II'], ['II'], [], 'division'),
+    Def('+', ['NN'], ['N'],  [], 'z+y'),
+    Def('-', ['NN'], ['N'],  [], 'z-y'),
+    Def('*', ['NN'], ['N'],  [], 'z*y'),
+    Def('%', ['NN'], ['NN'], [], 'division'),
 
     Def('!', ['NA'], [],  [], 'store value'),
     Def('@', ['A'],  ['N'],   [], 'fetch value'),
 
     Def('!+', ['NA'], ['A'],  [], 'store value, inc addr'),
-    Def('@+', ['A'],  ['AC'],   [], 'fetch value, inc addr'),
+    Def('@+', ['A'],  ['AN'],   [], 'fetch value, inc addr'),
 
-    Def('+!',  ['NA'], [],   [], 'increment variable by N')
+    Def('+!',  ['NA'], [],   [], 'increment variable by N'),
     Def('++',  ['A'],  [],   [], 'increment variable by 1'),
-    Def('-!',  ['NA'], [],   [], 'decrement variable by N')
+    Def('-!',  ['NA'], [],   [], 'decrement variable by N'),
     Def('--',  ['A'],  [],   [], 'decrement variable by 1'),
 
     Def('swap', ['ab'],  ['ba'],   [], 'swap top two items'),
@@ -42,3 +49,6 @@ words = [
     Def('[', [], [],    ['compiler:on'], 'begin a quote'),
     Def(']', [], ['Q'], ['compiler:off'], 'end a quote'),
 ]
+
+for word in words:
+    print word
