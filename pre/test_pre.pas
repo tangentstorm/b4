d66695c6bc33cef67828b2a0ac92b5ad3dab1137
tangentstorm <michal.wallace@gmail.com>
2013-09-01 14:27:09 -0500

    pre: replaced all uses of p() and ps() with the new 'array of variant' syntax.

--- pre/test_pre.pas
+++ pre/test_pre.pas
@@ -87,22 +87,20 @@ implementation uses pre;
 
   procedure test_seq_2;
   begin
-    pat := seq( ps( 7 ));
-    p( lit( 'once' ));
-    p( lit( ' ' ));
-    p( lit( 'upon' ));
-    p( lit( ' ' ));
-    p( lit( 'a' ));
-    p( lit( ' ' ));
-    p( lit( 'time' ));
+    pat := seq([
+      lit( 'once' ),
+      lit( ' ' ),
+      lit( 'upon' ),
+      lit( ' ' ),
+      lit( 'a' ),
+      lit( ' ' ),
+      lit( 'time' ) ]);
     should_consume( 'once upon a time', 'once upon a time' );
   end;
 
   procedure test_alt;
   begin
-    pat := alt( ps( 2 ));
-    p( lit( 'one' ));
-    p( lit( 'red' ));
+    pat := alt([ lit( 'one' ), lit( 'red' ) ]);
     should_consume( 'one',  'one fish' );
     should_consume( '',     'two fish' );
     should_consume( 'red',  'red fish' );
@@ -115,9 +113,7 @@ implementation uses pre;
     def( 'lower', any([ 'a'..'z' ]));
     def( 'digit', any([ '0'..'9' ]));
     def( 'upper', any([ 'A'..'Z' ]));
-    def( 'alpha', alt( ps( 2 )));
-    p( sub( 'upper' ));
-    p( sub( 'lower' ));
+    def( 'alpha', alt([ sub( 'upper' ), sub( 'lower' )]));
     def( 'alphas', rep( sub( 'alpha' )));
 
     pat := sub( 'alphas' );
@@ -130,9 +126,7 @@ implementation uses pre;
   procedure test_hex;
   begin
     // regexp to match a hex number in pascal
-    pat := seq( ps( 2 ));
-    p( lit( '$' ));
-    p( rep( any([ '0' .. '9', 'A'..'F', 'a'..'f' ])));
+    pat := seq([ lit( '$' ), rep( any([ '0' .. '9', 'A'..'F', 'a'..'f' ]))]);
     should_consume( '',         '1a2b3c' );
     should_consume( '$1a2b3c',  '$1a2b3c' );
   end;
