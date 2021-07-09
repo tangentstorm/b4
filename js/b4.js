16a120d6e5a658098ff84dba4a5ef892865cc2a2
tangentstorm <michal.wallace@gmail.com>
2021-07-09 18:46:53 -0400

    b4.js: bugfixes for second run, unknown words.

--- js/b4.js
+++ js/b4.js
@@ -6,14 +6,15 @@ let b4 = (new function() {
   cp=-1,ch='\x01',ib=[],wd='',            // lexer state
   compiling=0,state=[],target=[];         // compiler state
 
-  //function log(x) { document.body.innerHTML += `<p>${x}</p>` }
+  function tos() { return d.length? d[d.length-1] : undefined }
   function log(x) { console.log(x) }
+  let jsn=JSON.stringify
 
   function def(k,v) {
     let res=defs.length; defs.push(v); scope[0].push([k,res]); return res }
 
-  function nx() {
-    while (ib.length && (++cp>=ib[0].length)) {cp=0; ib.shift()}
+  function nx() { ++cp;
+    while (ib.length && (cp>=ib[0].length)) {cp=0; ib.shift()}
     return ch = (ib.length ? ib[0][cp] : EOF) }
 
   function word() { let res=[];
@@ -27,23 +28,24 @@ let b4 = (new function() {
         if (dict[j][0]===wd) { found=1; d.push(dict[j][1]) }}}
     return found }
 
-  function number() { let i = parseInt(wd,base);
-    return isNaN(i) ? 0 : d.push(i), 1 }
+  function number() {
+    let i = parseInt(wd,base);
+    if(isNaN(i))return 0; else { d.push(i); return 1 }}
 
-  function err(msg){ console.error(msg); throw msg }
+  function on(s) { ib=s; ch='\x01'; cp=-1; ib=[s] }
 
-  function run(x) { ib.push(x); var op, args=[], res='.', trace=[]
-    while (word()) {
+  function run(x) { on(x); var op, args=[], res='.', trace=[], d0
+    while (word()) { d0=jsn(d)
       op = findwd() ? defs[d.pop()]
          : number() ? (n=>()=>n)(d.pop())
-         : ()=> err(wd+'?');
+         : ()=>{ throw wd+'?' }
       if (compiling) { target.push(op) }
       else {
         args=[]; for (var i=0;i<op.length;i++) args.push(d.pop())
         res = op.apply(this, args)
         if(res!==undefined) d.push(res)}
-      trace.push({ wd, op:op.toString(), args:JSON.stringify(args),
-                   res, d:JSON.stringify(d) })}
+      trace.push({ d0, wd, op:op.toString(), args:jsn(args),
+                   res, d:jsn(d) })}
     console.table(trace) }
 
   var coreOps=[
@@ -54,9 +56,10 @@ let b4 = (new function() {
 
   scope.push(core)
   for (var i=0; i<coreOps.length;++i) def.apply(this, coreOps[i])
-  return { run, d }});
+  return { run, d, cp:()=>cp, ib, tos }});
 
 document.addEventListener("DOMContentLoaded", ()=>{
   b4.run('2 1 + 3 *');
+  b4.run('0 +');
   log = x=> { document.body.innerHTML+=`<p>${x}</p>` }
-  log(b4.d.pop()) });
+  log(b4.tos()) });
