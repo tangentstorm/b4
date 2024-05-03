b3ffa44b96e6965133cff7708c53eb1c0b4b1910
tangentstorm <michal.wallace@gmail.com>
2024-05-03 17:19:15 +0000

    js: move b4i into B4VM (formerly 'CPU')

--- js/b4.mjs
+++ js/b4.mjs
@@ -1,3 +1,5 @@
+// b4 virtual machine - javascript edition
+
 const
 LB = 128, LI = 129, DU = 130, SW = 131, OV = 132, ZP = 133,
 DC = 134, CD = 135, AD = 136, SB = 137, ML = 138, DV = 139,
@@ -22,6 +24,7 @@ op[TR]='tr', op[TK]='tk', op[TS]='ts', op[TL]='tl', op[TC]='tc',
 op[DB]='db', op[HL]='hl';
 for (let i=1;i<32;i++) op[i]='^'+String.fromCharCode(64+i)
 
+
 function asm(x) {
   if (x.match(/^-?[0-9A-F]+$/)) return parseInt(x,16)
   let res = op.indexOf(x)
@@ -31,13 +34,21 @@ function asm(x) {
 export function dis(x) {
   return op[x] || x.toString(16,0).padStart(2,'0').toUpperCase() }
 
-export class CPU {
+const hex=b=>b.toString(16,0).toUpperCase()
+
+function dump(xs) {
+  return `[${xs.map(hex).join(' ')}]` }
+
+export class B4VM {
   constructor() {
     this.ip = 0x100
     this.cs = []
     this.ds = []
     this.reg = new Array(32)
-    this.ram = new Uint8Array(4096).fill(0) }
+    this.ram = new Uint8Array(4096).fill(0)
+    this.out = console.log }
+
+  // microcode
   dtos() { return this.ds[this.ds.length-1]}
   dnos() { return this.ds[this.ds.length-2]}
   dpop() { return this.ds.pop()}
@@ -49,7 +60,8 @@ export class CPU {
     let res = []
     for (let i=0; i<len; i++) res.push(dis(this.ram[a++]))
     return res.join(' ')}
-
+
+  // arithmetic/logic ops
   ad() { return this.dput(this.dpop() + this.dpop()) }
   ml() { return this.dput(this.dpop() * this.dpop()) }
   sb() { let y=this.dpop(); return this.dput(this.dpop() -y) }
@@ -63,12 +75,16 @@ export class CPU {
   nt() { return this.dput(~this.dpop()) }
   eq() { let y=this.dpop(); return this.dput(-(this.dpop()==y)) }
   lt() { let y=this.dpop(); return this.dput(-(this.dpop()<y)) }
+
+  // stack ops
   zp() { this.dpop(); return this }
   du() { return this.dput(this.dtos()) }
   sw() { let y=this.dpop(),x=this.dpop(); return this.dput(y).dput(x) }
   ov() { return this.dput(this.dnos()) }
   dc() { return this.cput(this.dpop()) }
   cd() { return this.dput(this.cpop()) }
+
+  // memory ops
   wb() { this.ram[this.dpop()]=this.dpop(); return this }
   rb() { return this.dput(this.ram[this.dpop()]) }
   _ri(a0) {
@@ -85,6 +101,10 @@ export class CPU {
          return this.dput(RegY).du().ri().dput(4).ad().sw().wi() }
   wz() { this.dput(RegZ).ri().wi()
          return this.dput(RegZ).du().ri().dput(4).ad().sw().wi() }
+
+  // control ops
+  lb() { this.dput(this.ram[this.ip+++1]); }
+  li() { this.dput(this._ri(this.ip++)); this.ip+=3; }
   _go(a) { this.ip = Math.max(0x100,a)-1 }
   _i8(a) { let r=this.ram[a]; if (r>=0x80) r=-(r&0x7F)-1; return r }
   hp() { this._go(this.ip+this._i8(this.ip+1))}
@@ -95,84 +115,61 @@ export class CPU {
   nx() { if (this.ctos()>0) this.cput(this.cpop()-1)
          if (this.ctos()==0) { this.cpop(); this.ip++ }
          else this.hp() }
+
   step() {
     switch(this.ram[this.ip]){ // TODO: use a map of bound methods instead
-    case LB: this.dput(this.ram[this.ip+++1]); break
-// TODO:   case LI: this.dput(this._ri(this.ip++)); this.ip+=3; break
-    case DU: this.du(); break
-    case SW: this.sw(); break
-    case OV: this.ov(); break
-    case ZP: this.zp(); break
-    case DC: this.dc(); break
-    case CD: this.cd(); break
-    case AD: this.ad(); break
-    case SB: this.sb(); break
-    case ML: this.ml(); break
-    case DV: this.dv(); break
-    case MD: this.md(); break
-    case SH: this.sh(); break
-    case AN: this.an(); break
-    case OR: this.or(); break
-    case XR: this.xr(); break
-    case NT: this.nt(); break
-    case EQ: this.eq(); break
-    case LT: this.lt(); break
-    case JM: this.jm(); break
-    case HP: this.hp(); break
-    case H0: this.h0(); break
-    case CL: this.cl(); break
-    case RT: this.rt(); break
-    case NX: this.nx(); break
-    case RB: this.rb(); break
-    case WB: this.wb(); break
-    case RI: this.ri(); break
-    case WI: this.wi(); break
-    case RX: this.rx(); break
-    case RY: this.ry(); break
-    case WZ: this.wz(); break
-    case TG: this.tg(); break
-    case TA: this.ta(); break
-    case TW: this.tw(); break
-    case TR: this.tr(); break
-    case TK: this.tk(); break
-    case TS: this.ts(); break
-    case TL: this.tl(); break
+    case LB: this.lb(); break; case LI: thisli(); break
+    case DU: this.du(); break; case SW: this.sw(); break
+    case OV: this.ov(); break; case ZP: this.zp(); break
+    case DC: this.dc(); break; case CD: this.cd(); break
+    case AD: this.ad(); break; case SB: this.sb(); break
+    case ML: this.ml(); break; case DV: this.dv(); break
+    case MD: this.md(); break; case SH: this.sh(); break
+    case AN: this.an(); break; case OR: this.or(); break
+    case XR: this.xr(); break; case NT: this.nt(); break
+    case EQ: this.eq(); break; case LT: this.lt(); break
+    case JM: this.jm(); break; case HP: this.hp(); break
+    case H0: this.h0(); break; case CL: this.cl(); break
+    case RT: this.rt(); break; case NX: this.nx(); break
+    case RB: this.rb(); break; case WB: this.wb(); break
+    case RI: this.ri(); break; case WI: this.wi(); break
+    case RX: this.rx(); break; case RY: this.ry(); break
+    case WZ: this.wz(); break; case TG: this.tg(); break
+    case TA: this.ta(); break; case TW: this.tw(); break
+    case TR: this.tr(); break; case TK: this.tk(); break
+    case TS: this.ts(); break; case TL: this.tl(); break
     case TC: this.tc(); break
-    case DB: this.db(); break
-    case HL: this.hl(); break
+    case DB: this.db(); break; case HL: this.hl(); break
     default: break}
-    this.ip++}}
+    this.ip++}
+
+  b4i(line) {
+    if (line.startsWith('!')) {
+      let [a0,...xs] = line.split(/\s+/)
+      let a = parseInt(a0.slice(1), 16)
+      for (let x of xs) { this.ram[a++]=asm(x) }
+      return }
+    else for (let tok of line.split(' ')) {
+      switch (tok) {
+      case '?c': this.out(`cs: ${dump(this.cs)}`); break;
+      case '?d': this.out(`ds: ${dump(this.ds)}`); break;
+      case '?i': this.out(`ip: ${hex(this.ip)}`); break;
+      case '%q': process.exit(1); break;
+      case '%s': this.step(); break;
+      default:
+        if (tok.match(/^[0-9A-F]+$/)){
+          this.ds.push(parseInt(tok,16))}
+        else if (tok[0]=="'") {
+          if (tok.length==2) this.ds.push(tok[1].charCodeAt(0))
+          else if (tok.length==1) this.ds.push(32)
+          else this.out(`unknown command: ${tok}\n`)}
+        else if (tok[0]=="`" && tok.length==2) {
+          this.ds.push(4*(tok[1].charCodeAt(0)-64))}
+        else if (tok in this) { this[tok]() }
+        else if (tok[0]=="@") {
+          this.out(this.peek(parseInt(tok.slice(1),16), 16))  }
+        else this.out(`unknown command: ${tok}\n`)}}} }
+
+const vm = new B4VM()
 
-const b4 = new CPU()
-
-const hex=b=>b.toString(16,0).toUpperCase()
-
-function dump(xs) {
-  return `[${xs.map(hex).join(' ')}]` }
-
-export let out = console.log
-export function b4i(line){
-  if (line.startsWith('!')) {
-    let [a0,...xs] = line.split(/\s+/)
-    let a = parseInt(a0.slice(1), 16)
-    for (let x of xs) { b4.ram[a++]=asm(x) }
-    return }
-  else for (let tok of line.split(' ')) {
-    switch (tok) {
-    case '?c': out(`cs: ${dump(b4.cs)}`); break;
-    case '?d': out(`ds: ${dump(b4.ds)}`); break;
-    case '?i': out(`ip: ${hex(b4.ip)}`); break;
-    case '%q': process.exit(1); break;
-    case '%s': b4.step(); break;
-    default:
-      if (tok.match(/^[0-9A-F]+$/)){
-        b4.ds.push(parseInt(tok,16))}
-      else if (tok[0]=="'") {
-        if (tok.length==2) b4.ds.push(tok[1].charCodeAt(0))
-        else if (tok.length==1) b4.ds.push(32)
-        else out(`unknown command: ${tok}\n`)}
-      else if (tok[0]=="`" && tok.length==2) {
-        b4.ds.push(4*(tok[1].charCodeAt(0)-64))}
-      else if (tok in b4) { b4[tok]() }
-      else if (tok[0]=="@") { out(b4.peek(parseInt(tok.slice(1),16), 16))  }
-      else out(`unknown command: ${tok}\n`)}}}
+export function b4i(line){ vm.b4i(line) }
