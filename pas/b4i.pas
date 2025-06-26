e97e9e8575508218ea404f6081d9b099da832ef0
tangentstorm <michal.wallace@gmail.com>
2025-06-26 16:15:43 +0000

    pas/b4i: remove prompt now that we have b4ix

--- pas/b4i.pas
+++ pas/b4i.pas
@@ -2,19 +2,11 @@
 program b4i(input,output);
 uses ub4i, ub4;
 
-var prompt,line: string; done:boolean=false;
+var line: string; done:boolean=false;
 begin
-  if paramstr(1) = '-q' then prompt:=''
-  else begin
-    writeln('b4i.pas [',{$i %date%},'] ',
-            'type \h for help, \q to quit.');
-    prompt:='b4i> ';
-  end;
   rg^[RIP] := $100; rg^[regn('_')] := $100;
-  write(prompt);
   repeat
     readln(line);
     done := ub4i.b4i(line);
-    if not done then write(prompt);
   until done or eof;
 end.
